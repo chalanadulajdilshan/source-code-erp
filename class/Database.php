334802d227649fb6437053e924519ba317a978c7
sampath Dananjaya <56269465+sampathdp@users.noreply.github.com>
2025-08-25 10:31:40 +0530

    Update Database.php

--- class/Database.php
+++ class/Database.php
@@ -1,43 +1,58 @@
 <?php
-
-/**
-
- * Description of User
-
- *
-
- * @author sublime holdings
-
- * @web www.sublime.lk
-
- * */
 class Database
 {
-
-    private $host = 'localhost';
-    private $name = 'chalcepi_erp';
-    private $user = 'chalcepi_erp';
-    private $password = '}v+kRGNUPxtr';
-    public $DB_CON = '';
-
-    // private $host = 'localhost';
-    // private $name = 'source_code_erp';
-    // private $user = 'root';
-    // private $password = '';
-    // public $DB_CON = '';
-
+    private $host;
+    private $name;
+    private $user;
+    private $password;
+    public $DB_CON;
     public function __construct()
     {
+        // Detect environment
+        if ($this->isLocalServer()) {
+            // Local DB settings
+            $this->host = 'localhost';
+            $this->name = 'source_code_erp';
+            $this->user = 'root';
+            $this->password = '';
+        } else {
+            // Online DB settings
+            $this->host = 'localhost';
+            $this->name = 'chalcepi_erp';
+            $this->user = 'chalcepi_erp';
+            $this->password = '}v+kRGNUPxtr';
+            $this->DB_CON ='';
+        }
 
+        // Connect
         $this->DB_CON = mysqli_connect($this->host, $this->user, $this->password, $this->name);
+
+        if (!$this->DB_CON) {
+            die("Database connection failed: " . mysqli_connect_error());
+        }
+    }
+
+    private function isLocalServer()
+    {
+        // Method 1: Check hostname
+        if (in_array($_SERVER['SERVER_NAME'], ['localhost', '127.0.0.1'])) {
+            return true;
+        }
+
+        // Method 2: Check server IP (useful for CLI or cron jobs)
+        // if (php_sapi_name() === 'cli' && in_array(gethostname(), ['your-local-machine-name'])) {
+        //     return true;
+        // }
+
+        return false;
     }
 
     public function readQuery($query)
     {
-        $result = mysqli_query($this->DB_CON, $query) or die(mysqli_error());
-
+        $result = mysqli_query($this->DB_CON, $query) or die(mysqli_error($this->DB_CON));
         return $result;
     }
+
     public function escapeString($string)
     {
         return $this->DB_CON->real_escape_string($string);
